35aaf9ebb540b08ed2e375edabef2d9b4bfba39a
Laurie Barth <laurie@LauriesrkLaptop.fios-router.home>
2020-06-18 12:11:48 -0400

    Cypress initial setup

---
+++ cypress/e2e/test.js
@@ -0,0 +1,5 @@
+describe('test', () => {
+    it('can be tested', () => {
+        cy.visit('/')
+    })
+})
---
+++ cypress/support/index.js
@@ -0,0 +1,4 @@
+// Import commands.js using ES2015 syntax:
+import "@testing-library/cypress/add-commands"
+import "gatsby-cypress/commands"
+import "./commands"
