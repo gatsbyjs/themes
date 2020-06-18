3ae0ed75bd0548f6d61a44364269bad1f91940b1
Laurie Barth <laurie@LauriesrkLaptop.fios-router.home>
2020-06-18 12:32:38 -0400

    initial test for image

--- cypress/e2e/test.js
+++ cypress/e2e/test.js
@@ -1,5 +1,7 @@
-describe('test', () => {
-    it('can be tested', () => {
+describe('blog landing page', () => {
+    it('should display bio image', () => {
         cy.visit('/')
+          .get('picture > img')
+          .should('be.visible')
     })
 })
