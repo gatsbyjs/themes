8cd0319b5329dacf0b52d01bab7aed1da01eaaf4
Laurie Barth <laurie@LauriesrkLaptop.fios-router.home>
2020-06-19 08:28:49 -0400

    fix tests

--- cypress/e2e/blog-post.js
+++ cypress/e2e/blog-post.js
@@ -1,9 +1,11 @@
 describe(`hello world post`, () => {
   it(`should display date`, () => {
-    cy.visit(`/hello-world/`).getByText(`April 15, 2019`).should(`exist`)
+    cy.visit(`/hello-world/`)
+    cy.findByText(`April 15, 2019`).should(`exist`)
   })
 
   it(`should display bio`, () => {
-    cy.visit(`/hello-world/`).get(`picture > img`).should(`exist`)
+    cy.visit(`/hello-world/`)
+    cy.get(`picture > img`).should(`exist`)
   })
 })
