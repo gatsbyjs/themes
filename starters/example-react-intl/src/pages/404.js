a27497b246867b96edfebe4978c6cc295702054a
Matias Sallent <matias.sallent@mediamonks.com>
2020-08-04 14:52:00 -0300

    Add example 404 page to react-intl example

---
+++ starters/example-react-intl/src/pages/404.js
@@ -0,0 +1,16 @@
+import * as React from "react"
+import { useIntl } from "react-intl"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Page3 = () => {
+  const intl = useIntl()
+  return (
+    <Layout>
+      <SEO title="404" />
+      <h1>{intl.formatMessage({ id: "notFound" })}</h1>
+    </Layout>
+  )
+}
+
+export default Page3
