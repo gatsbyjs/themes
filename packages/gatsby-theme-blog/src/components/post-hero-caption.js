60d892858d100ae0b6726d30a2e75375cc2121c1
LekoArts <lekoarts@gmail.com>
2021-01-06 14:27:43 +0100

    fix(gatsby-theme-blog): Remove console.log

--- packages/gatsby-theme-blog/src/components/post-hero-caption.js
+++ packages/gatsby-theme-blog/src/components/post-hero-caption.js
@@ -1,27 +1,24 @@
 import React from "react"
 import { Styled, css, Flex } from "theme-ui"
 
-const PostHeroCaption = ({ text, url }) => {
-  console.log(__filename, `text`, { text, url })
-  return (
-    <>
-      {text && (
-        <Flex>
-          {url ? (
-            <Styled.a
-              css={css({ margin: `auto`, fontStyle: `italic` })}
-              href={url}
-              target="_blank"
-            >
-              {text}
-            </Styled.a>
-          ) : (
-            <Styled.i css={css({ margin: `auto` })}>{text}</Styled.i>
-          )}
-        </Flex>
-      )}
-    </>
-  )
-}
+const PostHeroCaption = ({ text, url }) => (
+  <>
+    {text && (
+      <Flex>
+        {url ? (
+          <Styled.a
+            css={css({ margin: `auto`, fontStyle: `italic` })}
+            href={url}
+            target="_blank"
+          >
+            {text}
+          </Styled.a>
+        ) : (
+          <Styled.i css={css({ margin: `auto` })}>{text}</Styled.i>
+        )}
+      </Flex>
+    )}
+  </>
+)
 
 export default PostHeroCaption
