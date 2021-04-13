c52df45c0d13de3b4f46e2d9cb16fa313dd35b5b
LekoArts <lekoarts@gmail.com>
2021-04-13 14:13:40 +0200

    rename seo component + add ts types

--- starters/example-i18n/src/pages/404.js
+++ starters/example-i18n/src/pages/404.js
@@ -1,12 +1,12 @@
 import * as React from "react"
 import { LocalizedLink } from "gatsby-theme-i18n"
 import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Seo from "../components/seo"
 
 const NotFound = () => {
   return (
     <Layout>
-      <SEO title="404 - Page Not Found" />
+      <Seo title="404 - Page Not Found" />
       <h1>404</h1>
       <p>Page Not Found</p>
       <p>
